745e190720aba1504e911f997ae59fbe1b5f5b2f
frizehero <bag4sk4r4@gmail.com>
2023-11-13 09:50:39 +0700

    W12: Soal 1

---
+++ week-12/README.md
@@ -0,0 +1,42 @@
+# Praktikum 1: Mengunduh Data dari Web Service (API)
+
+### Langkah 1: Buat Project Baru
+
+Buatlah sebuah project flutter baru dengan nama books di folder src week-12 repository GitHub Anda.
+
+Kemudian Tambahkan dependensi http dengan mengetik perintah berikut di terminal.
+
+### Langkah 2: Cek file pubspec.yaml
+
+Jika berhasil install plugin, pastikan plugin http telah ada di file pubspec ini seperti berikut.
+
+### Langkah 3: Buka file main.dart
+
+### Soal 1
+
+Tambahkan nama panggilan Anda pada title app sebagai identitas hasil pekerjaan Anda.
+
+![This is an alt text.](./docs/praktikum1_1.jpg "gambar")
+
+### Langkah 4: Tambah method getData()
+
+Tambahkan method ini ke dalam class \_FuturePageState yang berguna untuk mengambil data dari API Google Books.
+
+### Soal 2
+
+- Carilah judul buku favorit Anda di Google Books, lalu ganti ID buku pada variabel path di kode tersebut. Caranya ambil di URL browser Anda seperti gambar berikut ini.
+
+- Kemudian cobalah akses di browser URI tersebut dengan lengkap seperti ini. Jika menampilkan data JSON, maka Anda telah berhasil. Lakukan capture milik Anda dan tulis di README pada laporan praktikum. Lalu lakukan commit dengan pesan "W12: Soal 2".
+
+![This is an alt text.](./docs/praktikum1_2.jpg "gambar")
+
+### Langkah 5: Tambah kode di ElevatedButton
+
+Tambahkan kode pada onPressed di ElevatedButton seperti berikut.
+
+### Soal 3
+
+- Jelaskan maksud kode langkah 5 tersebut terkait substring dan catchError!
+- Capture hasil praktikum Anda berupa GIF dan lampirkan di README. Lalu lakukan commit dengan pesan "W12: Soal 3".
+
+![This is an alt text.](./docs/praktikum1_hasil.gif "gambar")
---
+++ week-12/src/books/lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo Wira',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const FuturePage(),
+    );
+  }
+}
+
+class FuturePage extends StatefulWidget {
+  const FuturePage({super.key});
+
+  @override
+  State<FuturePage> createState() => _FuturePageState();
+}
+
+class _FuturePageState extends State<FuturePage> {
+  String result = '';
+
+  Future<Response> getData() async {
+    const authority = 'www.googleapis.com';
+    const path = '/books/v1/volumes/2MA8DwAAQBAJ';
+    Uri url = Uri.https(authority, path);
+    return http.get(url);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Back from the Future Wira'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {});
+                getData().then((value) {
+                  result = value.body.toString().substring(0, 450);
+                  setState(() {});
+                }).catchError((_) {
+                  result = 'An error occured';
+                  setState(() {});
+                });
+              },
+              child: const Text("GO!"),
+            ),
+            const Spacer(),
+            Text(result),
+            const Spacer(),
+            const CircularProgressIndicator(),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
